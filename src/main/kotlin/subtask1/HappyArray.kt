6df894f6137079b892a6637c1573b62bb49017fc
Eugene Kos <mstrsid@live.com>
2021-05-22 14:58:43 +0300

    feat: all tasks done

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,31 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
+
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var happyArray: IntArray = sadArray
+        while (checkOnSad(happyArray)) {
+            var tempArray = intArrayOf()
+            tempArray+=happyArray.first()
+            for (i in 1 until happyArray.lastIndex) {
+                if (happyArray[i] < happyArray[i - 1] + happyArray[i + 1]) {
+                    tempArray += happyArray[i]
+                } else {
+                    continue
+                }
+            }
+            tempArray+=happyArray.last()
+            happyArray = tempArray
+        }
+        return happyArray
+    }
+
+    private fun checkOnSad(tempArray: IntArray): Boolean{
+        var isSad = false
+        for(i in 1 until tempArray.lastIndex) {
+            isSad = tempArray[i] > tempArray[i - 1] + tempArray[i + 1]
+            if(isSad) break
+        }
+        return isSad
     }
 }
--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -2,9 +2,9 @@ package subtask2
 
 class BillCounter {
 
-    // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
-    fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+    fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String = when (val annSum = (bill.sum()- bill[k])/2) {
+        b -> "Bon Appetit"
+        else -> (b-annSum).toString()
     }
 }
