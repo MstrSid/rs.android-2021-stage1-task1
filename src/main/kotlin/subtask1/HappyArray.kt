3dc5cc9140661eab6f5f83db8e0b5d73e6ec7217
Eugene Kos <mstrsid@live.com>
2021-05-26 23:22:05 +0300

    feat: optimize happyarray code

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,29 +4,16 @@ class HappyArray {
 
 
     fun convertToHappy(sadArray: IntArray): IntArray {
-        var happyArray: IntArray = sadArray
-        while (checkOnSad(happyArray)) {
-            var tempArray = intArrayOf()
-            tempArray+=happyArray.first()
+        val happyArray = sadArray.toMutableList()
+        outer@ while (true) {
             for (i in 1 until happyArray.lastIndex) {
-                if (happyArray[i] < happyArray[i - 1] + happyArray[i + 1]) {
-                    tempArray += happyArray[i]
-                } else {
-                    continue
+                if (happyArray[i] > happyArray[i - 1] + happyArray[i + 1]) {
+                    happyArray.removeAt(i)
+                    continue@outer
                 }
             }
-            tempArray+=happyArray.last()
-            happyArray = tempArray
+            break
         }
-        return happyArray
-    }
-
-    private fun checkOnSad(tempArray: IntArray): Boolean{
-        var isSad = false
-        for(i in 1 until tempArray.lastIndex) {
-            isSad = tempArray[i] > tempArray[i - 1] + tempArray[i + 1]
-            if(isSad) break
-        }
-        return isSad
+        return happyArray.toIntArray()
     }
 }
